b7632b29f33ef904fa9f6008c28a0999a9a1d331
sokil <sokililona@gmail.com>
2025-09-04 16:42:10 +0300

    Added checkTemperature method

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
 public class Main {
     static String name = "Ilona";
     static int age = 23;
     static double height = 1.68;
     static boolean isQA = true;
+
 
     public static void main(String[] args) {
         securityCheckBuyAge();
@@ -14,24 +16,86 @@ public class Main {
         printHelloWorld();
         linearSearch();
         checkTemperature();
+        userCalculator();
+        printStudents();
+        showStudentNames();
+        showCities();
+        divideNumbers(10, 2);
+        divideNumbers(5, 0);
+        checkLoginTest();
     }
+    public static void checkLoginTest() {
+        String expected = "Login successful";
+        String actual = "Login successful";
+
+        if (expected.equals(actual)) {
+            System.out.println("Тест пройдено");
+        } else {
+            System.out.println("Тест не пройдено");
+        }
+    }
+
+
+    public static void divideNumbers(int a, int b) {
+        try {
+            int result = a / b;
+            System.out.println("Результат: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("На нуль ділити не можна");
+        }
+    }
+
+    public static void showCities() {
+        ArrayList<String> cities = new ArrayList<>();
+
+        cities.add("Тернопіль");
+        cities.add("Бровари");
+        cities.add("Яремче");
+        cities.add("Львів");
+
+        System.out.println(cities);
+    }
+
+
+    public static void printStudents() {
+        String[] students = {"Ілона", "Cлавік", "Інеса", "Лєра", "Марійка"};
+
+        for (int i = 0; i < students.length; i++) {
+            System.out.println(students[i]);
+        }
+    }
+
+    public static void showStudentNames() {
+        String[] students = {"Олег", "Андрій", "Софія", "Катерина", "Дмитро"};
+
+        for (String student : students) {
+            System.out.println(student);
+        }
+    }
+
+    public static void userCalculator() {
+        int result = Calculator.add(5, 7);
+        System.out.println("Результат: " + result);
+    }
+
     public static void checkTemperature() {
         int temperature = -1;
         String result = (temperature > 0) ? "плюс" : "мінус";
         System.out.println("Температура: " + result);
     }
-public static void linearSearch() {
-    int[] numbers = {10, 20, 30, 40, 50};
-    int target = 30;
 
-    int result = linearSearch(numbers, target);
+    public static void linearSearch() {
+        int[] numbers = {10, 20, 30, 40, 50};
+        int target = 40;
+
+        int result = linearSearch(numbers, target);
 
         if (result != -1) {
-        System.out.println("Елемент знайдено, індекс: " + result);
-    } else {
-        System.out.println("Елемент не знайдено");
+            System.out.println("Елемент знайдено, індекс: " + result);
+        } else {
+            System.out.println("Елемент не знайдено");
+        }
     }
-}
 
     public static void printHelloWorld() {
         System.out.println("Hello, World!");
@@ -80,6 +144,7 @@ public static void linearSearch() {
 
         }
     }
+
     public static int linearSearch(int[] array, int target) {
         for (int i = 0; i < array.length; i++) {
             if (array[i] == target) {
